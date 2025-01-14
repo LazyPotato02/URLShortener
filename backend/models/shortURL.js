47bdb9f6b3bc937ae651db82cafd77d87678d0d9
LazyPotato <74728512+LazyPotato02@users.noreply.github.com>
2025-01-14 22:22:39 +0200

    Changed model file

--- backend/models/shortURL.js
+++ backend/models/shortURL.js
@@ -1,22 +1,10 @@
 const mongoose = require("mongoose");
 
-(async () => {
-    const { customAlphabet } = await import("nanoid");
-    const generateId = customAlphabet('0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz', 10);
 
-    const shortenerSchema = mongoose.Schema({
-        id: { type: String, required: true },
-        url: { type: String, required: true },
-    }, { timestamps: true });
+const shortenerSchema = mongoose.Schema({
+    url: {type: String, required: true},
+}, {timestamps: true});
 
-    shortenerSchema.pre('save', function (next) {
-        if (!this.id) {
-            this.id = generateId();
-        }
-        next();
-    });
+const ShortUrls = mongoose.model("ShortUrls", shortenerSchema);
 
-    const ShortUrls = mongoose.model("ShortUrls", shortenerSchema);
-
-    module.exports = ShortUrls;
-})();
+module.exports = ShortUrls;
